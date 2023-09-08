d66e0c055b2e622cf04aeb6331b5148030681536
Magnus E. Halvorsen <magnus.halvorsen@testify.no>
2023-09-08 21:49:30 +0200

    Rettet Cucumber-scenarier for helsesjekk

--- cucumber/features/health.feature
+++ cucumber/features/health.feature
@@ -5,14 +5,14 @@ Egenskap: Helseindikatorer
     Scenario: Applikasjonen skal gi status OK hvis den kjører
         Gitt at applikasjonen kjører
         Når jeg kaller helsesjekk for applikasjonen
-        Så skal den returnere status "I'm OK"
+        Så skal den returnere status for applikasjonen: "I'm OK"
     
     Scenario: Applikasjonen skal gi status OK for Yr hvis Yr svarer OK på status
-        Gitt at Yr ikke returnerer status OK
-        Når jeg kaller helsesjekk for applikasjonen
-        Så skal den returnere status "Yr is OK"
+        Gitt at Yr returnerer status OK
+        Når jeg kaller helsesjekk for Yr
+        Så skal den returnere status for Yr: "Yr is OK"
 
     Scenario: Applikasjonen skal gi status ikke OK hvis Yr ikke svarer OK på status
         Gitt at Yr ikke returnerer status OK
-        Når jeg kaller helsesjekk for applikasjonen
-        Så skal den returnere status "Yr is not OK :("
+        Når jeg kaller helsesjekk for Yr
+        Så skal den returnere status for Yr: "Yr is not OK :("
--- cucumber/features/steps/health.ts
+++ cucumber/features/steps/health.ts
@@ -1,20 +1,37 @@
 import { Given, When, Then } from "@cucumber/cucumber"
-import { getHealth } from "./api/sut"
+import { getHealth, getYrHealth } from "./api/sut"
 import { assert } from "chai"
 
 interface HealthStatus {
     status: string
 }
 let healthStatus: HealthStatus | undefined = undefined
+let yrHealthStatus: HealthStatus | undefined = undefined
 
 Given('at applikasjonen kjører', () => {
     return true
 })
 
+Given('at Yr returnerer status OK', function () {
+    return true
+});
+
+Given('at Yr ikke returnerer status OK', function () {
+    return true
+});
+
 When('jeg kaller helsesjekk for applikasjonen', async () => {
     healthStatus = await getHealth()
 })
 
-Then('skal den returnere status {string}', (status) => {
+When('jeg kaller helsesjekk for Yr', async () => {
+    yrHealthStatus = await getYrHealth()
+})
+
+Then('skal den returnere status for applikasjonen: {string}', (status) => {
     assert.equal(healthStatus?.status, status)
+})
+
+Then('skal den returnere status for Yr: {string}', (status) => {
+    assert.equal(yrHealthStatus?.status, status)
 })
