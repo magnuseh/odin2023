17a611107e1be089c2fdde3c4f5c0e853e60c515
Magnus E. Halvorsen <magnus.halvorsen@testify.no>
2023-09-18 15:31:05 +0200

    Støtte for å konfigurere annen instans av WireMock enn localhost

--- cucumber-kotlin/src/test/kotlin/api/WireMock.kt
+++ cucumber-kotlin/src/test/kotlin/api/WireMock.kt
@@ -3,11 +3,15 @@ package api
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMockBuilder
 
+val wiremockScheme: String = System.getProperty("wiremock.scheme", "https")
+val wiremockHost: String = System.getProperty("wiremock.host", "localhost")
+val wiremockPort: Int = System.getProperty("wiremock.port", "8443").toInt()
+
 object WireMock {
     private val wireMock = WireMockBuilder()
-        .scheme("https")
-        .host("localhost")
-        .port(8443)
+        .scheme(wiremockScheme)
+        .host(wiremockHost)
+        .port(wiremockPort)
         .build();
 
     fun get(): WireMock { return wireMock }
